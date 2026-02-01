fd1a1a656aa28e3614a6fd6efc8e87be3502c78c
Devsingh0123 <dayamns20@gmail.com>
2026-02-02 02:13:57 +0530

    update assects folder in frontend and make categories file for food data

---
+++ frontend/src/categories.js
@@ -0,0 +1,26 @@
+import snacks from "./assets/snacks.jpg"
+import mainCourse from "./assets/main course.jpg"
+import desserts from "./assets/desserts.jpg"
+import pizza from "./assets/pizza.jpg"
+import burger from "./assets/burger.jpg"
+import sandwich from "./assets/sandwich.jpg"
+import southIndian from "./assets/south indian.jpg"
+import northIndian from "./assetsnorth indian.jpg"
+import chinese from "./assets/chinese.jpg"
+import fastFood from "./assets/fast food.avif"
+import all from "./assets/all.webp"
+
+const categories = [
+    {"category":"All", "image":all },
+    {"category":"Snacks", "image": snacks},
+    {"category":"Main Course", "image":mainCourse },
+    {"category":"Desserts", "image":desserts },
+    {"category":"Pizza", "image":pizza },
+    {"category":"Burgers", "image":burger },
+    {"category":"Sanwiches", "image": sandwich},
+    {"category":"South Indian", "image":southIndian },
+    {"category":"North Indian", "image":northIndian },
+    {"category":"Chinese", "image":chinese },
+    {"category":"Fast Food", "image": fastFood},
+    
+]
